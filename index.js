9d5408d7d8a74c9cace96a6d55f88e08184867f2
Thuanjimmy <76500464+Thuanjimmy@users.noreply.github.com>
2021-04-27 22:29:48 -0400

    Loading - V1

---
+++ index.js
@@ -0,0 +1,7 @@
+// $("button").on("click", function(){
+//     $("#qwe").animate({opacity: 0.2});
+// })
+
+$("button").on("click", function(){
+    $("#qwe").removeClass("line").delay(1200).addClass("line2");
+})
